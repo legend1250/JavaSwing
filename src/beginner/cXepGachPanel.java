4e77770d7b7cfec240f1e54c915aa3a3be4981c2
Vinh <Vinh@DESKTOP-PUMHLTF>
2016-10-10 23:24:35 +0700

    working...

--- src/beginner/cXepGachPanel.java
+++ src/beginner/cXepGachPanel.java
@@ -8,6 +8,7 @@ import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class cXepGachPanel extends JPanel{
 
@@ -29,7 +30,7 @@ public class cXepGachPanel extends JPanel{
 					g.setColor(Color.WHITE);	//background color
 				}
 				if( brick[i][j]==1){
-					g.setColor(Color.BLUE);	//next brick background color
+					g.setColor(Color.RED);	//next brick background color
 				}
 				if( brick[i][j]==2){
 					g.setColor(Color.GREEN);	//next brick background color
@@ -60,6 +61,7 @@ public class cXepGachPanel extends JPanel{
 	Random rd = new Random();
 	int puzzle = 0,count=0;
 	boolean firstP = false;
+	Timer t;
 	
 	public cXepGachPanel(){
 		setLayout(null);
@@ -73,12 +75,10 @@ public class cXepGachPanel extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				nextBrick();
+				newPiece();
+				t.start();
 			}
 		});
-		
-		
-		
 		
 		//draw background
 		for(int i = 0; i < nRow ;i++){
@@ -87,13 +87,44 @@ public class cXepGachPanel extends JPanel{
 			}
 		}
 		
+		t = new Timer(200, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				move();
+				
+			}
+		});
 	}
 	
-	public void nextBrick(){
-		int n = rd.nextInt(2);
+	
+	public void newPiece(){
+		int n = 0;
+		n=rd.nextInt(5);
+		if(!firstP){
+			addPiece(rd.nextInt(5));
+			showNextPiece(n);
+			setnumPiece(n);
+			firstP = true;
+		}
+		else{
+			addPiece(getnumPiece());
+			showNextPiece(n);
+			setnumPiece(n);
+		}
+		
+	}
+	
+	public void showNextPiece(int n){
 		n+=1;
-		setPuzzle(n);
-		//clear 
+		String s[] = {
+				"square",
+				"L",
+				"I",
+				"Z",
+				"T",
+		};
 		for(int i = 0; i < 3 ;i++){
 			for(int j = 0 ; j < 4 ; j++){
 				nextbrick[i][j] = 0;
@@ -101,10 +132,55 @@ public class cXepGachPanel extends JPanel{
 		}
 		
 		if( n==1){	//hinh vuông
-			nextbrick[1][1] = n;
-			nextbrick[1][2] = n;
-			nextbrick[2][1] = n;
-			nextbrick[2][2] = n;
+			nextbrick[1][1] = 1;
+			nextbrick[1][2] = 1;
+			nextbrick[2][1] = 1;
+			nextbrick[2][2] = 1;
+		}
+		else if(n==2){	//hinh L
+			nextbrick[0][1] = 1;
+			nextbrick[1][1] = 1;
+			nextbrick[2][1] = 1;
+			nextbrick[2][2] = 1;
+		}
+		else if(n==3){	//hinh I
+			nextbrick[1][0] = 1;
+			nextbrick[1][1] = 1;
+			nextbrick[1][2] = 1;
+			nextbrick[1][3] = 1;
+		}
+		else if(n==4){	//hinh Z
+			nextbrick[1][1] = 1;
+			nextbrick[1][2] = 1;
+			nextbrick[2][0] = 1;
+			nextbrick[2][1] = 1;
+		}
+		else if(n==5){	//hinh T
+			nextbrick[2][1] = 1;
+			nextbrick[2][2] = 1;
+			nextbrick[2][3] = 1;
+			nextbrick[1][2] = 1;
+		}
+		
+		System.out.println("Next: "+s[n-1]);
+		
+		repaint();
+	}
+	
+	public void addPiece(int n){
+		n+=1;
+		String s[] = {
+				"square",
+				"L",
+				"I",
+				"Z",
+				"T",
+		};
+		for(int i = 0 ; i < 4 ; i++){
+			arr[i][0] = 0;
+			arr[i][1] = 0;
+		}
+		if( n==1){	//hinh vuông
 			arr[0][0] = 0;
 			arr[0][1] = 8;
 			arr[1][0] = 0;
@@ -113,56 +189,85 @@ public class cXepGachPanel extends JPanel{
 			arr[2][1] = 8;
 			arr[3][0] = 1;
 			arr[3][1] = 9;
-			
 		}
 		else if(n==2){	//hinh L
-			nextbrick[0][1] = n;
-			nextbrick[1][1] = n;
-			nextbrick[2][1] = n;
-			nextbrick[2][2] = n;
 			arr[0][0] = 0;
 			arr[0][1] = 8;
 			arr[1][0] = 1;
 			arr[1][1] = 8;
 			arr[2][0] = 2;
 			arr[2][1] = 8;
+			arr[3][0] = 2;
+			arr[3][1] = 9;
+		}
+		else if(n==3){	//hinh I
+			arr[0][0] = 0;
+			arr[0][1] = 7;
+			arr[1][0] = 0;
+			arr[1][1] = 8;
+			arr[2][0] = 0;
+			arr[2][1] = 9;
+			arr[3][0] = 0;
+			arr[3][1] = 10;
+		}
+		else if(n==4){	//hinh Z
+			arr[0][0] = 0;
+			arr[0][1] = 8;
+			arr[1][0] = 0;
+			arr[1][1] = 9;
+			arr[2][0] = 1;
+			arr[2][1] = 7;
+			arr[3][0] = 1;
+			arr[3][1] = 8;
+		}
+		else if(n==5){	//hinh T
+			arr[0][0] = 1;
+			arr[0][1] = 8;
+			arr[1][0] = 1;
+			arr[1][1] = 9;
+			arr[2][0] = 1;
+			arr[2][1] = 10;
 			arr[3][0] = 0;
 			arr[3][1] = 9;
-			
 		}
 		
-		String s[] = {
-				"square",
-				"L",
-				"I",
-				"Z",
-				"T",
-		};
-		System.out.println(s[n-1]);
-		newBrick();
+		
+		for(int i = 0 ; i < 4 ; i++){
+			brick[arr[i][0]][arr[i][1]] = 1;
+		}
+		System.out.println("Show: " +s[n-1]);
 		repaint();
 	}
 	
-	public void setPuzzle(int n){
+	public void move(){
+		
+		
+		//
+		for(int i = 0 ; i < 4 ; i++){
+			brick[arr[i][0]][arr[i][1]] = 0;
+		}
+		
+		for(int i = 0 ; i < 4 ; i++){
+			arr[i][0] +=1;
+			if(arr[i][0] >= nRow){
+				//arr[i][0] +=30;
+				newPiece();
+			}
+		}
+		
+		for(int i = 0 ; i < 4 ; i++){
+			brick[arr[i][0]][arr[i][1]] = 1;
+		}
+		
+		repaint();
+		
+	}
+	
+	public void setnumPiece(int n){
 		this.puzzle = n;
 	}
 	
-	public int getPuzzle(){
+	public int getnumPiece(){
 		return puzzle;
-	}
-	
-	public void newBrick(){
-		int P = getPuzzle();
-		
-		if(P==1){
-			brick[arr[0][0]][arr[0][1]] = P;
-			brick[arr[1][0]][arr[1][1]] = P;
-			brick[arr[2][0]][arr[2][1]] = P;
-			brick[arr[3][0]][arr[3][1]] = P;
-			
-		}
-		
-		repaint();
-		
 	}
 }
