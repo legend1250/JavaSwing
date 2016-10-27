72c4f74b489cb985c877b21ed6696720a90bdd9e
Vinh <vinh_nguyen1211@yahoo.com.vn>
2016-10-27 19:19:31 +0700

    Update WeeklyExam07

--- src/weekyexam/cWE07C_Prog01_T151487.java
+++ src/weekyexam/cWE07C_Prog01_T151487.java
@@ -69,7 +69,7 @@ public class cWE07C_Prog01_T151487 extends JFrame{
 				// TODO Auto-generated method stub
 				String []s;
 				s = txtInput.getText().split("\\s+");
-				if(s.length > 6){
+				if(s.length != 6){
 					JOptionPane.showMessageDialog(null, "Invalid input. Input 6 Integer number between 0 and 400");
 				}
 				else{
