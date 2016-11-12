4a724a8b1450a8e2d5326a912c2e056524211be6
Vinh <vinh_nguyen1211@yahoo.com.vn>
2016-11-12 18:03:42 +0700

    new file

---
+++ src/beginner/test6.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package beginner;
+
+/**
+ * @author Vinh - legend1250
+ *
+ */
+public class test6 {
+	int a = 0;
+	public static void main(String[] args){
+		show(" dsajdnsakjdbaskjbd ");
+	}
+	
+	public static void show(String x){
+		System.out.println("x: "+x);
+	}
+}
